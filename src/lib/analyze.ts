55ee60ca8d7b0edc55bd9c53b312841a503a0e47
Jesse Anderson <jeryanders@gmail.com>
2023-11-14 08:06:15 -0500

    fix: pipeline

--- src/lib/analyze.ts
+++ src/lib/analyze.ts
@@ -61,6 +61,7 @@ export async function analyze(
   await printReport(analytics)
   const folder = 'complexity-assessment'
   const filename = `${folder}/${analytics.repository.repo}/${context.sha}-${analytics.actor}-infrastructure.json`
+  core.info(`Report saved to: ${filename}`)
   if (!existsSync(folder)) await mkdir(folder)
   if (!existsSync(`${folder}/${analytics.repository.repo}`))
   await writeFile(filename, JSON.stringify(analytics, undefined, 2))
